2a44b48d8348af318cda308bac4e62b00e1189a4
Govind maithil <govindmaithilrtn@gmail.com>
2022-07-04 11:14:17 +0530

    Added : Player position Check

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -6,13 +6,45 @@ public class SnakeAndLadder {
 	
 	public static void main(String[] args) {
 		
+		final int No_Play = 0;
+		final int Ladder = 1;
+		final int Snake = 2;
+		
+		
+		
 		int Starting_position = 0;
+		int Player_position = 0;
 		System.out.println("Starting position is :" + Starting_position);
 		
 		Random random = new Random();
-   	    int	Rolling_Die = random.nextInt(6)+1;
-   	    System.out.println(Rolling_Die);
-   	   
-		
+   	    int	Rolling_Dice = random.nextInt(6)+1;
+   	    System.out.println("After Rolling dice "+ Rolling_Dice);
+   	    
+   	    int PlayerCheck = random.nextInt(3);
+   	    System.out.println("Player check (0)No_Play , (1) Ladder , (2) Snake "+ PlayerCheck );
+   	    
+   	    if (PlayerCheck == 0)
+   	    {
+   	    	System.out.println("your position is not change, you are at position " + Player_position);
+   	    }
+   	    
+   	    else if (PlayerCheck == 1) 
+   	    {
+   	    	Player_position = Player_position + Rolling_Dice;
+    		System.out.println(" 'Congratulation' you got the ladder your position will be increesed by " + Rolling_Dice);
+   	    }
+   	    
+   	    else if (PlayerCheck == 2)
+   	    {
+   	    	Player_position = Player_position - Rolling_Dice;
+    		System.out.println(" 'Opps' you got the Snake your position will be Decrease by " + Rolling_Dice);
+   	    }
+   	    else 
+   	    {
+   	    	System.out.println(" 'Sorry' Somethis went Worng");
+   	    	
+   	    }
+   	    	
 	}
 }
+  
