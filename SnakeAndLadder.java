d346fa5c015dc6d5aa7399e507f9cbd2db06bc78
Govind maithil <govindmaithilrtn@gmail.com>
2022-07-04 09:00:40 +0530

    Added: Snake and ladder problem

---
+++ SnakeAndLadder.java
@@ -0,0 +1,10 @@
+package bridgelabz.com;
+
+public class SnakeAndLadder {
+	
+	public static void main(String[] args) {
+		
+		int Starting_position = 0;
+		System.out.println("Starting position is :-" + Starting_position);
+	}
+}
