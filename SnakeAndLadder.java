cb1b9debb8133f860403641c829b690b068116d6
Govind maithil <govindmaithilrtn@gmail.com>
2022-07-04 09:24:56 +0530

    Added: Rooling_die in given range

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,10 +1,18 @@
 package bridgelabz.com;
 
+import java.util.Random;
+
 public class SnakeAndLadder {
 	
 	public static void main(String[] args) {
 		
 		int Starting_position = 0;
 		System.out.println("Starting position is :" + Starting_position);
+		
+		Random random = new Random();
+   	    int	Rolling_Die = random.nextInt(6)+1;
+   	    System.out.println(Rolling_Die);
+   	   
+		
 	}
 }
